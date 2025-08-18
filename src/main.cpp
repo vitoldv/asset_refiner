5756ebb29b1c82d68a2e0f5d08689ec504330f06
VITOLD DESKTOP <v.vitaliy.vishnyakov@gmail.com>
2025-08-18 13:49:00 +0300

    Added glfw3 library and implemented window creation along with basic update loop

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,46 @@
 #include <iostream>
+#include <string>
+
+#include "GLFW/glfw3.h"
+
+#define WINDOW_WIDTH	800
+#define WINDOW_HEIGHT	800
+#define WINDOW_TITLE	"AssetRefiner"
+
+#define IS_FPS_LIMIT	true
+#define TARGET_FPS		60
+
+GLFWwindow* window;
+// Frame time control 
+int previousFrameTime = 0;
+int currentFrameTime = 0;
+float deltaTime;
 
 int main()
 {
-	std::cout << "Hello. It's me, AssetRefiner!" << std::endl;
+	glfwInit();
+	// Set GLFW to NOT work with OpenGL
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+	window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, nullptr, nullptr);
+
+	float frameTime = 0;
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+
+		currentFrameTime = glfwGetTime() * 1000.0f;
+		frameTime = currentFrameTime - previousFrameTime;
+
+		float TARGET_FRAME_TIME = 1000.0f / TARGET_FPS;
+		if (IS_FPS_LIMIT && frameTime < TARGET_FRAME_TIME) continue;
+
+		deltaTime = frameTime / 1000.0f;
+		previousFrameTime = currentFrameTime;
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
+
 	return 0;
 }
